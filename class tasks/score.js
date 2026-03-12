ba88de484c986b13120cf25d0d38c7a6ed711ad4
khulordavailla-creator <khulordavailla@gmail.com>
2026-03-12 15:03:16 +0400

    sdsd

---
+++ class tasks/score.js
@@ -0,0 +1,6 @@
+function calculateaverage() {
+  const ragaca = document.querySelector("form");
+
+  const ragacaa = (math + english) / 2;
+  document.getElementById("result").innerHTML = "student" + "name" + "average";
+}
--- davalebebis folder/davaleaba4.js
+++
@@ -1,7 +0,0 @@
-function ragaca() {
-  const result = document.querySelector("P");
-
-  const num1 = document.querySelector("num1").value;
-  const num2 = document.querySelector("num2").value;
-  result.innerHTML = num1 + num2;
-}
